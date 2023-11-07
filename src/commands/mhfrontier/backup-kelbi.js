4d21277261deb1764e5017b0f9db8c5e40ae7236
Gustavo Ragazzi Amorim <ragazzi@hotmail.com.br>
2023-11-07 16:15:50 -0300

    Feat: Add local backup for complete database

--- src/commands/mhfrontier/backup-kelbi.js
+++ src/commands/mhfrontier/backup-kelbi.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js')
 const { exec } = require('child_process')
 const AdmZip = require('adm-zip')
+const fs = require('fs')
 const email = require('../../utils/backupKelbiEmail')
 const pool = require('../../utils/dbConnection')
 const bcrypt = require('bcryptjs')
@@ -36,7 +37,23 @@ module.exports = {
 
             // Discord AND outlook can't take questlist data becase 8MB limite
             if (blacklistInput) {
-              interaction.editReply('The file exceeds the 8MB limit.')
+              interaction.editReply('The file exceeds the 8MB limit. Saving backup locally')
+              const outputFolderPath = './kelbi-backups'
+
+              if (!fs.existsSync(outputFolderPath)) {
+                fs.mkdirSync(outputFolderPath, { recursive: true })
+              }
+
+              const destinationPath = `${outputFolderPath}/${zipFileName}`
+
+              fs.rename(zipFileName, destinationPath, (error) => {
+                if (error) {
+                  console.error('Failed to move .zip file to output folder: ', error)
+                  fs.unlinkSync(zipFileName)
+                }
+
+                fs.unlinkSync(backupFileName)
+              })
             } else {
               interaction.editReply({
                 content: 'Backup was done successfully! Also sending it by email',
